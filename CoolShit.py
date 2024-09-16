2adf5f056286c852653c939acd8292e571c01a3f
zachyjello <110743525+zachyjello@users.noreply.github.com>
2024-09-16 14:40:51 -0400

    Git ignore and basic functions

--- CoolShit.py
+++ CoolShit.py
@@ -12,24 +12,26 @@ def cozmo_program(robot: cozmo.robot.Robot):
     robot.play_anim_trigger(cozmo.anim.Triggers.PetDetectionDog).wait_for_completed()
     robot.play_anim_trigger(cozmo.anim.Triggers.Surprise).wait_for_completed()
     
+#Makes cozmo raise head, lower arms, and speak
+def cozmo_speak(robot: cozmo.robot.Robot, textToSay):
+    raise_head_action = robot.set_head_angle(angle=25)
+    lift_action = robot.set_lift_height(0.0, in_parallel=True)
+    robot.say_text(textToSay).wait_for_completed()
 
-def cozmo_speak(robot: cozmo.robot.Robot):
+def cozmo_showImage(robot: cozmo.robot.Robot, imageName, duration):
     current_directory = os.path.dirname(os.path.realpath(__file__))    
-    sdk_png = os.path.join(current_directory, "..", "Tutorial", "face_images", "cozmosdk.png")
-    hello_png = os.path.join(current_directory, "..", "Tutorial", "face_images", "cozmosdk.png")
+    image_png = os.path.join(current_directory, "..", "Tutorial", "face_images", imageName + ".png")
+        
+    image_settings = (image_png, Image.BICUBIC)
     
-    image_settings = [(sdk_png, Image.BICUBIC),
-                      (hello_png, Image.NEAREST)]
-    face_images = []
-    for image_name, resampling_mode in image_settings:
-        image = Image.open(image_name)
-
-        # resize to fit on Cozmo's face screen
-        resized_image = image.resize(cozmo.oled_face.dimensions(), resampling_mode)
-
-        # convert the image to the format used by the oled screen
-        face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image,
+    image = Image.open(image_png)
+    resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
+    face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image,
                                                                  invert_image=True)
-        face_images.append(face_image)
+    robot.display_oled_face_image(face_image, duration * 1000.0)
+    
+def comzo_takePicture(robot: cozmo.robot.Robot):
+    robot.set_head_angle(angle=10.0).wait_for_completed()
+    robot.set_lift_height(0.0).wait_for_completed()
 
 cozmo.run_program(cozmo_program)
