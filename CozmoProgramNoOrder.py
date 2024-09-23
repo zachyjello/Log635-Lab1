4dce934c5277917dccb03d42cc535627cb79af29
Nicolas Gaulin-Métivier <nicolas.gaulin-metivier.1@ens.etsmtl.ca>
2024-09-23 18:00:48 -0400

    Base is working

--- CozmoProgramNoOrder.py
+++ CozmoProgramNoOrder.py
@@ -9,19 +9,64 @@ alreadyDoneImages = []
 liveCamera = False
 fullPath = "photos"
 
+def createAllDirectories():
+    # Indiquer le dossier pour stocker les photos
+    if not os.path.exists('photos'):
+        os.makedirs('photos')
+    if not os.path.exists('photos/Triangles'):
+        os.makedirs('photos/Triangles')
+    if not os.path.exists('photos/Triangles/Triangle2'):
+        os.makedirs('photos/Triangles/Triangle2')
+    if not os.path.exists('photos/Triangles/Triangle3'):
+        os.makedirs('photos/Triangles/Triangle3')
+    if not os.path.exists('photos/Triangles/Triangle4'):
+        os.makedirs('photos/Triangles/Triangle4')
+    if not os.path.exists('photos/Triangles/Triangle5'):
+        os.makedirs('photos/Triangles/Triangle5')
+    if not os.path.exists('photos/Hexagons'):
+        os.makedirs('photos/Hexagons')
+    if not os.path.exists('photos/Hexagons/Hexagon2'):
+        os.makedirs('photos/Hexagons/Hexagon2')
+    if not os.path.exists('photos/Hexagons/Hexagon3'):
+        os.makedirs('photos/Hexagons/Hexagon3')
+    if not os.path.exists('photos/Hexagons/Hexagon4'):
+        os.makedirs('photos/Hexagons/Hexagon4')
+    if not os.path.exists('photos/Hexagons/Hexagon5'):
+        os.makedirs('photos/Hexagons/Hexagon5')
+    if not os.path.exists('photos/Diamonds'):
+        os.makedirs('photos/Diamonds')
+    if not os.path.exists('photos/Diamonds/Diamond2'):
+        os.makedirs('photos/Diamonds/Diamond2')
+    if not os.path.exists('photos/Diamonds/Diamond3'):
+        os.makedirs('photos/Diamonds/Diamond3')
+    if not os.path.exists('photos/Diamonds/Diamond4'):
+        os.makedirs('photos/Diamonds/Diamond4')
+    if not os.path.exists('photos/Diamonds/Diamond5'):
+        os.makedirs('photos/Diamonds/Diamond5')
+    if not os.path.exists('photos/Circles'):
+        os.makedirs('photos/Circles')
+    if not os.path.exists('photos/Circles/Circle2'):
+        os.makedirs('photos/Circles/Circle2')
+    if not os.path.exists('photos/Circles/Circle3'):
+        os.makedirs('photos/Circles/Circle3')
+    if not os.path.exists('photos/Circles/Circle4'):
+        os.makedirs('photos/Circles/Circle4')
+    if not os.path.exists('photos/Circles/Circle5'):
+        os.makedirs('photos/Circles/Circle5')
+
 def on_new_camera_image(evt, **kwargs):    
     global liveCamera
     global fullPath
     if liveCamera:
         print("Cozmo is taking a photo")
         pilImage = kwargs['image'].raw_image
-        global directory
-        pilImage.save(fullPath, "JPG")
+        #TODO: Modify path to increment photo_01, photo_02, ...
+        pilImage.save(fullPath, "JPEG")
 
 def TakePhoto(robot: cozmo.robot.Robot, path):
     global liveCamera
     global fullPath
-    fullPath = path
+    fullPath=path
     # Assurez-vous que la tête et le bras de Cozmo sont à un niveau raisonnable
     robot.set_head_angle(degrees(10.0)).wait_for_completed()
     robot.set_lift_height(0.0).wait_for_completed()
@@ -30,20 +75,21 @@ def TakePhoto(robot: cozmo.robot.Robot, path):
     time.sleep(0.1)    
     liveCamera = False
 
-def action_for_marker_0(marker_idrobot: cozmo.robot.Robot):
-    #Do a flip
-    nothing = True
+
+#TALK
+def action_for_marker_0(robot: cozmo.robot.Robot):
     print("I do action 1")
+    robot.say_text("My name is Jeff").wait_for_completed()
 
+#EMOTION
 def action_for_marker_1(robot: cozmo.robot.Robot):
-    #Buckle your shoes
-    nothing = True
     print("I do action 2")
+    robot.play_anim_trigger(cozmo.anim.Triggers.Surprise).wait_for_completed()
 
+#STACK CUBES
 def action_for_marker_2(robot: cozmo.robot.Robot):
-    #My name is Jeff
-    nothing = True
     print("I do action 3")
+
 
 def action_for_marker_3(robot: cozmo.robot.Robot):
     #Fart sound
@@ -97,113 +143,41 @@ def action_for_marker_15(robot: cozmo.robot.Robot):
     #Enfin
     nothing = True
 
-
-def handle_object_appeared(evt, **kw):
-    global alreadyDoneImages
-    # Cela sera appelé chaque fois qu'un EvtObjectAppeared est déclanché
-    # chaque fois qu'un objet entre en vue
-    if isinstance(evt.obj, CustomObject):
-        print(f"Cozmo started seeing a type: {str(evt.obj.object_type)} id: {str(evt.obj.object_id)}")
-
-    robot= cozmo.robot.Robot
-    if alreadyDoneImages.index(evt.obj.marker_type) != ValueError:
-        if evt.obj.marker_type == CustomObjectMarkers.Triangles2:
-            TakePhoto(robot, "photos/Triangles/Triangle2/photo_01.jpg")
-            action_for_marker_0(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Triangles2)
-        elif evt.obj.marker_type == CustomObjectMarkers.Hexagons3:
-            TakePhoto(robot, "photos/Hexagons/Hexagon3/photo_01.jpg")
-            action_for_marker_1(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Hexagons3)
-        elif evt.obj.marker_type == CustomObjectMarkers.Circles5:
-            TakePhoto(robot, "photos/Circles/Circle5/photo_01.jpg")
-            action_for_marker_2(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Circles5)
-        elif evt.obj.marker_type == CustomObjectMarkers.Diamonds4:
-            TakePhoto(robot, "photos/Diamonds/Diamond4/photo_01.jpg")
-            action_for_marker_3(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Diamonds4)
-        elif evt.obj.marker_type == CustomObjectMarkers.Triangles4:
-            TakePhoto(robot, "photos/Triangles/Triangle4/photo_01.jpg")
-            action_for_marker_4(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Triangles4)
-        elif evt.obj.marker_type == CustomObjectMarkers.Diamonds5:
-            TakePhoto(robot, "photos/Diamonds/Diamond2/photo_01.jpg")
-            action_for_marker_5(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Diamonds5)
-        elif evt.obj.marker_type == CustomObjectMarkers.Circles2:
-            TakePhoto(robot, "photos/Circles/Circle2/photo_01.jpg")
-            action_for_marker_6(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Circles2)
-        elif evt.obj.marker_type == CustomObjectMarkers.Hexagons2:
-            TakePhoto(robot, "photos/Hexagons/Hexagon2/photo_01.jpg")
-            action_for_marker_7(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Hexagons2)
-        elif evt.obj.marker_type == CustomObjectMarkers.Diamonds2:
-            TakePhoto(robot, "photos/Diamonds/Diamond2/photo_01.jpg")
-            action_for_marker_8(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Diamonds2)
-        elif evt.obj.marker_type == CustomObjectMarkers.Circles4:
-            TakePhoto(robot, "photos/Circles/Circle4/photo_01.jpg")
-            action_for_marker_9(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Circles4)
-        elif evt.obj.marker_type == CustomObjectMarkers.Hexagons4:
-            TakePhoto(robot, "photos/Hexagons/Hexagon4/photo_01.jpg")
-            action_for_marker_10(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Hexagons4)
-        elif evt.obj.marker_type == CustomObjectMarkers.Triangles3:
-            TakePhoto(robot, "photos/Triangles/Triangle3/photo_01.jpg")
-            action_for_marker_11(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Triangles3)
-        elif evt.obj.marker_type == CustomObjectMarkers.Triangles5:
-            TakePhoto(robot, "photos/Triangles/Triangle5/photo_01.jpg")
-            action_for_marker_12(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Triangles5)
-        elif evt.obj.marker_type == CustomObjectMarkers.Diamonds3:
-            TakePhoto(robot, "photos/Diamonds/Diamond3/photo_01.jpg")
-            action_for_marker_13(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Diamonds3)
-        elif evt.obj.marker_type == CustomObjectMarkers.Circles3:
-            TakePhoto(robot, "photos/Circles/Circle3/photo_01.jpg")
-            action_for_marker_14(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Circles3)
-        elif evt.obj.marker_type == CustomObjectMarkers.Hexagons5:
-            TakePhoto(robot, "photos/Hexagons/Hexagon5/photo_01.jpg")
-            action_for_marker_15(robot)
-            alreadyDoneImages.append(CustomObjectMarkers.Hexagons5)
-        else:
-            print("I saw something but it's not a marker?")
-
-    
-
-
-
 def cozmo_program(robot: cozmo.robot.Robot):
-    global current_step
+    createAllDirectories()
     # Gestionnaires d'évennements à chaque fois que Cozmo vois un objet
-    robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
+    #robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
     # Chaque fois que Cozmo voit une "nouvelle" image, prends une photo
     robot.add_event_handler(cozmo.world.EvtNewCameraImage, on_new_camera_image)
 
-    # Indiquer le dossier pour stocker les photos
-    global directory    
-    directory = f"{strftime('%y%m%d')}"
-    if not os.path.exists('photos'):
-        os.makedirs('photos')
-    if not os.path.exists('photos/Triangles'):
-        os.makedirs('photos/Triangles')
-    if not os.path.exists('photos/Hexagons'):
-        os.makedirs('photos/Hexagons')
-    if not os.path.exists('photos/Diamonds'):
-        os.makedirs('photos/Diamonds')
-    if not os.path.exists('photos/Circles'):
-        os.makedirs('photos/Circles')
+    #Création des objets customs
+    robot.world.define_custom_wall(CustomObjectTypes.CustomType00,
+                                   CustomObjectMarkers.Triangles2,
+                                   50, 50, 10, 10, True)
+    robot.world.define_custom_wall(CustomObjectTypes.CustomType01,
+                                   CustomObjectMarkers.Circles2,
+                                   50, 50, 10, 10, True)
+    #TODO: Create you custom object
 
-    while object_found < 16:
+    
+    while len(alreadyDoneImages) < 16:
         # Chercher les objets
         lookaround = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-        object_found = robot.world.wait_until_observe_num_objects(num=16, timeout=60) #LOOK TIMEOUTS
+        object_found = robot.world.wait_until_observe_num_objects(num=1, timeout=60) #LOOK TIMEOUTS
         lookaround.stop()
+
+        for obj in object_found:
+            # Vérifiez si l'objet est un marqueur que vous surveillez
+            if obj.object_type not in alreadyDoneImages:
+                if obj.object_type == CustomObjectTypes.CustomType00:
+                    TakePhoto(robot, "photos/Triangles/Triangle2/photo_01.jpg") #TODO: For each picture make sure you're in front, so hardcode some position
+                    action_for_marker_0(robot)
+                    alreadyDoneImages.append(CustomObjectTypes.CustomType00)
+                if obj.object_type == CustomObjectTypes.CustomType01:
+                    TakePhoto(robot, "photos/Circles/Circle2/photo_01.jpg")
+                    action_for_marker_1(robot)
+                    alreadyDoneImages.append(CustomObjectTypes.CustomType01)
+                #TODO: ADD your step here
 
     while True:
         time.sleep(0.1)
