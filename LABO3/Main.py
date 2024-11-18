2aee92cf791b9a1ddbcbce877d4c30ea39cd36c2
fatmaalj4 <fatmafatouma98@icloud.com>
2024-11-18 14:59:23 -0500

    update

--- LABO3/Main.py
+++ LABO3/Main.py
@@ -1,19 +1,15 @@
 import cozmo
 from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes, ObservableElement, ObservableObject, LightCube1Id, LightCube2Id, LightCube3Id
 from cozmo.util import Pose, degrees, distance_mm, speed_mmps, Vector3
-from Scene import Scene
+from LABO3.Scene import Scene
 #from Scenario import Scenario
 #from Rules import Rules
 
 #Main
-def cozmo_program():
-    Scene.create_walls(cozmo.robot.Robot)
+def cozmo_program(robot : cozmo.robot.Robot):
+    Scene.create_walls(robot)
     Scene.plot_walls()
-<<<<<<< HEAD
-    Scene.posDepart(cozmo.robot.Robot)
-=======
     Scene.placeHint()
->>>>>>> 8edee3308bcd570d21c2d2d1b7df6f254e7dbcab
-
-
+    Scene.posDepart(robot)
+    
 cozmo.run_program(cozmo_program, use_3d_viewer=True, use_viewer=True)
